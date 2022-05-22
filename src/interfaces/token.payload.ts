64dcbd0182a8799f5a3b4039e92fabb9366390d6
hitachi567 <abidai790@gmail.com>
2022-05-22 12:40:08 +0300

    token payload class were added

---
+++ src/interfaces/token.payload.ts
@@ -0,0 +1,31 @@
+import { IsString, IsBoolean, IsNotEmpty, IsOptional } from 'class-validator';
+import { HttpStatus, ApiError } from '@ducksclan/wrapper-express';
+import { Validation } from '@ducksclan/utils';
+
+export default class TokenPayload {
+    @IsString()
+    @IsNotEmpty()
+    user_id: string;
+
+    @IsOptional()
+    @IsBoolean()
+    admin?: boolean;
+
+    constructor(object: TokenPayload) {
+        this.user_id = object?.user_id;
+        this.admin = object?.admin;
+    }
+}
+
+export async function checkPayload(
+    payload: TokenPayload
+): Promise<TokenPayload> {
+    let object = new TokenPayload(payload);
+    let errors = await Validation.validate(object);
+
+    if (errors.length > 0) {
+        throw new ApiError('invalid token', HttpStatus.Unauthorized);
+    }
+
+    return object;
+}
