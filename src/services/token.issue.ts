966369d623f68607e98717a9c938b5d3f48122d5
hitachi567 <abidai790@gmail.com>
2022-06-03 22:21:24 +0300

    TokenIsue service was added

---
+++ src/services/token.issue.ts
@@ -0,0 +1,36 @@
+import { ExpiresInOptions, TokensPair } from '@ducksclan/wrapper-express';
+import { Account, RefreshToken } from '../entities';
+import { jwt } from '..';
+import AbstractRepository from '../repositories/abstract.repository';
+import TokenPayload from '../interfaces/token.payload';
+
+const expiresIn: ExpiresInOptions = {
+    access: '10m',
+    refresh: '7d',
+};
+
+export default class TokenIssue extends AbstractRepository {
+    async issue(
+        account: Account,
+        fingerprint: string,
+        ip?: string
+    ): Promise<TokensPair> {
+        let payload: TokenPayload = {
+            user_id: account.id,
+            access_level: account.access_level,
+        };
+
+        let pair = jwt.generateTokensPair(payload, expiresIn);
+
+        let refreshToken = RefreshToken.init(
+            fingerprint,
+            account,
+            pair.refresh,
+            ip
+        );
+
+        refreshToken = await this.manager.save(refreshToken);
+
+        return pair;
+    }
+}
