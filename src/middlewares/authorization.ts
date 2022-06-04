64854284011f12d03af25f64d548f253a666cd79
hitachi567 <abidai790@gmail.com>
2022-06-04 17:05:17 +0300

    authorization was added

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,44 @@
+import { TaggedLoclas } from '@ducksclan/wrapper-express';
+import { Validation } from '@ducksclan/utils';
+import {
+    asyncMiddleware,
+    ClientError,
+    Middleware,
+    Request,
+} from '@ducksclan/wrapper-express';
+import TokenPayload from '../interfaces/token.payload';
+import jwt from '../services/jwt';
+
+export interface AuthoriziedLoacls extends TokenPayload, TaggedLoclas {}
+
+export default function authorization(): Middleware<any, AuthoriziedLoacls> {
+    return asyncMiddleware(async (request, response, next) => {
+        let payload = jwt.verifyAccess(getToken(request));
+        let errors = await Validation.validate(payload);
+
+        if (errors.length > 0) {
+            throw ClientError.Unauthorized('invalid token payload');
+        }
+
+        response.locals.user_id = payload.user_id;
+        response.locals.access_level = payload.access_level;
+
+        next();
+    });
+}
+
+function getToken(request: Request): string {
+    let authorization = request.headers.authorization?.split(' ');
+
+    if (!authorization) {
+        throw ClientError.BadRequest('authorization header required');
+    }
+
+    let isMatch = authorization[0].match(/bearer/i);
+
+    if (!isMatch) {
+        throw ClientError.BadRequest('bearer token required');
+    }
+
+    return authorization[1];
+}
