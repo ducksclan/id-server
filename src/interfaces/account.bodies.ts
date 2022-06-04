dbedfa911f4e5e5caf538d853a93dafebecec780
hitachi567 <abidai790@gmail.com>
2022-06-04 17:04:52 +0300

    AccountGetBody was added

--- src/interfaces/account.bodies.ts
+++ src/interfaces/account.bodies.ts
@@ -5,3 +5,11 @@ export interface LoginBody {
 export interface CodeBody {
     code: string;
 }
+
+export interface AccountGetBody {
+    user_id: string;
+    email: string;
+    created_at: Date;
+    verified_at: Date | null;
+    user_status: 'Not Verified' | 'Normal' | 'Admin' | 'Super Admin';
+}
